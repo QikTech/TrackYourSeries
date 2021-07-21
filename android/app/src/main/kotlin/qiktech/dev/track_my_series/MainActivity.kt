90157abdc3123fba906f582066f875dce57f2de2
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-21 13:49:04 +0530

    TVA | Timeline Reset

---
+++ android/app/src/main/kotlin/qiktech/dev/track_my_series/MainActivity.kt
@@ -0,0 +1,6 @@
+package qiktech.dev.track_my_series
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- track_my_series/android/app/src/main/kotlin/com/example/track_my_series/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.track_my_series
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
